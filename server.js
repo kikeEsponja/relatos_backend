5c194ee5daae650bf35a2d53c98a40fe30d245e1
Alejandro <element3999@gmail.com>
2025-10-04 22:10:20 -0300

    agregando un relato

--- server.js
+++ server.js
@@ -13,21 +13,36 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Conectado a MongoDB"))
   .catch(err => console.error("❌ Error de conexión:", err));
 
-const Relato = mongoose.model("Relato", {
+const RelatoSchema = new mongoose.Schema({
   titulo: String,
+  autor: String,
   contenido: String,
-  autor: String
+  fecha: String
+});
+
+const Relato = mongoose.model("Relato", RelatoSchema);
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando correctamente");
 });
 
 app.get("/relatos", async (req, res) => {
-  const relatos = await Relato.find();
-  res.json(relatos);
+  try{
+    const relatos = await Relato.find().sort({ fecha: -1 });
+    res.json(relatos);
+  }catch (error) {
+    res.status(500).json({ error: "Error al intentar obtener los relatos" })
+  }
 });
 
 app.post("/relatos", async (req, res) => {
-  const nuevo = new Relato(req.body);
-  await nuevo.save();
-  res.json({ ok: true });
+  try{
+    const nuevo = new Relato(req.body);
+    await nuevo.save();
+    res.json({ ok: true });
+  }catch(error){
+    res.status(500).json({ error: "Error al guardar el relato" });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
