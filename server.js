cedaede83ae84a151ebd625d43b2ad416e0dd52a
Alejandro <element3999@gmail.com>
2025-10-04 23:47:22 -0300

    sin pistas

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const RelatoSchema = new mongoose.Schema({
   titulo: String,
 });
 
-const Relato = mongoose.model(RelatoSchema);
+const Relato = mongoose.model("relatos", RelatoSchema);
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando correctamente");
